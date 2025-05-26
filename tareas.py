a856c98005b3de976ba45cfd4cff9e5dfb612461
bill-vi <william03cadavid@gmail.com>
2025-05-25 23:03:39 -0500

    Juan: agrega clase Tareas con funcionalidad básica

--- tareas.py
+++ tareas.py
@@ -1,1 +1,11 @@
-print("Gestor de tareas")
+# tareas.py
+class Tareas:
+    def __init__(self, nombre, descripcion, fecha_vencimiento):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.fecha_vencimiento = fecha_vencimiento
+        self.id = None  # Para asignar un ID único más adelante
+
+    def mostrar_tarea(self):
+        print(f"{self.id}. Tarea: {self.nombre}, Descripción: {self.descripcion}, Fecha de Vencimiento: {self.fecha_vencimiento}")
+
